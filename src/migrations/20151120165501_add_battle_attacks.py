3ec702d7d9dc8e9e97a9de243ef0d0be7cc4d855
butterscotchstallion <sasswrench@gmail.com>
2015-11-20 18:44:49 -0500

    Adds some initial stuff for battles Fixes #1 - Players can attack themselves and implements a fun solution!

---
+++ src/migrations/20151120165501_add_battle_attacks.py
@@ -0,0 +1,14 @@
+"""
+a caribou migration
+
+name: add_battle_attacks 
+version: 20151120165501
+"""
+
+def upgrade(connection):
+    # add your upgrade step here
+    pass
+
+def downgrade(connection):
+    # add your downgrade step here
+    pass
