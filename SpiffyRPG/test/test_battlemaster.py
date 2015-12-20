bf1c8ac3d9ac1a445a32b3800a24aa7f4bae3e71
butterscotchstallion <sasswrench@gmail.com>
2015-12-20 18:33:39 -0500

    Adds try/except for possibility of units dying before round limit is reached

--- SpiffyRPG/test/test_battlemaster.py
+++ SpiffyRPG/test/test_battlemaster.py
@@ -59,97 +59,105 @@ class TestBattlemaster(unittest.TestCase):
         """
         Round 1
         """
-        attacker_weapon = self._make_item(item_type="rock")
-        target_weapon = self._make_item(item_type="scissors")
+        try:
+            attacker_weapon = self._make_item(item_type="rock")
+            target_weapon = self._make_item(item_type="scissors")
 
-        combatant_1.equip_item(item=attacker_weapon)
-        combatant_2.equip_item(item=target_weapon)
+            combatant_1.equip_item(item=attacker_weapon)
+            combatant_2.equip_item(item=target_weapon)
 
-        hit_info = combatant_1.attack(target=combatant_2)
+            hit_info = combatant_1.attack(target=combatant_2)
 
-        battle.add_round(attacker=combatant_1,
-                         target=combatant_2,
-                         hit_info=hit_info)
+            battle.add_round(attacker=combatant_1,
+                             target=combatant_2,
+                             hit_info=hit_info)
 
-        self.assertEqual(len(battle.rounds), 1)
+            self.assertEqual(len(battle.rounds), 1)
 
-        rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
-        rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
+            rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
+            rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
 
-        self.assertEqual(rounds_won_for_combatant_1, 1)
-        self.assertEqual(rounds_won_for_combatant_2, 0)
+            self.assertEqual(rounds_won_for_combatant_1, 1)
+            self.assertEqual(rounds_won_for_combatant_2, 0)
 
-        """
-        Round 2
-        """
-        round_2_attacker_weapon = self._make_item(item_type="paper")
-        round_2_target_weapon = self._make_item(item_type="rock")
+            """
+            Round 2
+            """
+            round_2_attacker_weapon = self._make_item(item_type="paper")
+            round_2_target_weapon = self._make_item(item_type="rock")
 
-        combatant_1.equip_item(item=round_2_attacker_weapon)
-        combatant_2.equip_item(item=round_2_target_weapon)
+            combatant_1.equip_item(item=round_2_attacker_weapon)
+            combatant_2.equip_item(item=round_2_target_weapon)
 
-        hit_info = combatant_1.attack(target=combatant_2)
+            hit_info = combatant_1.attack(target=combatant_2)
 
-        battle.add_round(attacker=combatant_2,
-                         target=combatant_1,
-                         hit_info=hit_info)
-
-        self.assertEqual(len(battle.rounds), 2)
-
-        rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
-        rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
-
-        self.assertEqual(rounds_won_for_combatant_1, 1)
-        self.assertEqual(rounds_won_for_combatant_2, 1)
-
-        """
-        Test that units cannot attack twice in a row
-        """
-        with self.assertRaises(ValueError):
             battle.add_round(attacker=combatant_2,
                              target=combatant_1,
                              hit_info=hit_info)
 
-            """
-            Rounds should not change because adding a new
-            round with the same attacker as last round 
-            should not be possible
-            """
             self.assertEqual(len(battle.rounds), 2)
 
-        """
-        Round 3
-        """
-        round_3_attacker_weapon = self._make_item(item_type="lizard")
-        round_3_target_weapon = self._make_item(item_type="spock")
+            rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
+            rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
 
-        combatant_1.equip_item(item=round_3_attacker_weapon)
-        combatant_2.equip_item(item=round_3_target_weapon)
+            self.assertEqual(rounds_won_for_combatant_1, 1)
+            self.assertEqual(rounds_won_for_combatant_2, 1)
 
-        hit_info = combatant_1.attack(target=combatant_2)
+            """
+            Test that units cannot attack twice in a row
+            """
+            with self.assertRaises(ValueError):
+                battle.add_round(attacker=combatant_2,
+                                 target=combatant_1,
+                                 hit_info=hit_info)
 
-        battle.add_round(attacker=combatant_1,
-                         target=combatant_2,
-                         hit_info=hit_info)
+                """
+                Rounds should not change because adding a new
+                round with the same attacker as last round 
+                should not be possible
+                """
+                self.assertEqual(len(battle.rounds), 2)
 
-        self.assertEqual(len(battle.rounds), 3)
+            """
+            Round 3
+            """
+            round_3_attacker_weapon = self._make_item(item_type="lizard")
+            round_3_target_weapon = self._make_item(item_type="spock")
 
-        rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
-        rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
+            combatant_1.equip_item(item=round_3_attacker_weapon)
+            combatant_2.equip_item(item=round_3_target_weapon)
 
-        self.assertEqual(rounds_won_for_combatant_1, 2)
-        self.assertEqual(rounds_won_for_combatant_2, 1)
+            hit_info = combatant_1.attack(target=combatant_2)
 
-        """
-        Make sure we can't add a new round and that
-        attempting to do so will raise a ValueError
-        """
-        with self.assertRaises(ValueError):
             battle.add_round(attacker=combatant_1,
                              target=combatant_2,
                              hit_info=hit_info)
 
             self.assertEqual(len(battle.rounds), 3)
+
+            rounds_won_for_combatant_1 = battle.get_rounds_won(combatant=combatant_1)
+            rounds_won_for_combatant_2 = battle.get_rounds_won(combatant=combatant_2)
+
+            self.assertEqual(rounds_won_for_combatant_1, 2)
+            self.assertEqual(rounds_won_for_combatant_2, 1)
+
+            """
+            Make sure we can't add a new round and that
+            attempting to do so will raise a ValueError
+            """
+            with self.assertRaises(ValueError):
+                battle.add_round(attacker=combatant_1,
+                                 target=combatant_2,
+                                 hit_info=hit_info)
+
+                self.assertEqual(len(battle.rounds), 3)
+        except ValueError:
+            """
+            If we're here, it means that one of the combatants died
+            before three rounds were over. This is quite possible, since
+            units are randomly generated and could be in different ranges
+            """
+            self.assertTrue(combatant_1.is_dead() or combatant_2.is_dead())
 
 if __name__ == '__main__':
     unittest.main()
