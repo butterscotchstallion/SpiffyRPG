503f450a60884d824529f3a6bfaa8a69d7b0f79b
butterscotchstallion <sasswrench@gmail.com>
2015-12-26 17:43:13 -0500

    Adds some assertions to Worldbuilder test

--- SpiffyRPG/test/test_worldbuilder.py
+++ SpiffyRPG/test/test_worldbuilder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
-from SpiffyWorld import Worldbuilder, Database
+from SpiffyWorld import Worldbuilder, Database, World
 
 
 class TestWorldbuilder(unittest.TestCase):
@@ -18,7 +18,14 @@ class TestWorldbuilder(unittest.TestCase):
         worldbuilder = Worldbuilder(db=self.db, irc="quux")
         world = worldbuilder.build_world()
 
+        self.assertIsInstance(world, World)
         self.assertTrue(world.dungeon_collection)
+        self.assertTrue(world.dungeon_collection.dungeons)
+
+        for dungeon in world.dungeon_collection.dungeons:
+            self.assertTrue(dungeon.name)
+            self.assertTrue(dungeon.units)
+
         self.assertTrue(world.unit_collection)
         self.assertTrue(world.effect_collection)
         self.assertTrue(world.dialogue_collection)
