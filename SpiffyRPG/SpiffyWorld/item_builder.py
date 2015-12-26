43779a1c1cd3efd5922f224baba8220ee083699d
butterscotchstallion <sasswrench@gmail.com>
2015-12-25 21:58:22 -0500

    Fixes problem with applying item effects in ItemBuilder

--- SpiffyRPG/SpiffyWorld/item_builder.py
+++ SpiffyRPG/SpiffyWorld/item_builder.py
@@ -13,17 +13,18 @@ class ItemBuilder:
     def build_items(self, **kwargs):
         item_effects_map = kwargs["item_effects_map"]
         item_models = kwargs["item_models"]
+        effect_collection = kwargs["effect_collection"]
         items = []
 
         for item_model in item_models:
-            effects = []
+            item_effects = []
 
             item_id = item_model["id"]
 
             if item_id in item_effects_map:
-                effects = item_effects_map[item_id]
+                item_effects = item_effects_map[item_id]
 
-            item_model["effects"] = effects
+            item_model["effects"] = effect_collection.get_effects_by_effect_id_list(item_effects)
 
             item = self.build_item(item_model=item_model)
 
