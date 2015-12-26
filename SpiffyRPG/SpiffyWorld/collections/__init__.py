b0c5bae2586e65a9f6620235d7130b57f96fdf63
butterscotchstallion <sasswrench@gmail.com>
2015-12-26 12:21:40 -0500

    Adds DungeonCollection import

--- SpiffyRPG/SpiffyWorld/collections/__init__.py
+++ SpiffyRPG/SpiffyWorld/collections/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from .unit_collection import UnitCollection
 from .item_collection import ItemCollection
 from .effect_collection import EffectCollection
@@ -7,3 +8,4 @@ from .unit_effect_collection import UnitEffectCollection
 from .unit_dialogue_collection import UnitDialogueCollection
 from .dungeon_unit_collection import DungeonUnitCollection
 from .unit_type_collection import UnitTypeCollection
+from .dungeon_collection import DungeonCollection
