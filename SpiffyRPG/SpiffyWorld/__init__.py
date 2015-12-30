10ba764ed73c1489af4b5bc61ff0db647620c229
butterscotchstallion <sasswrench@gmail.com>
2015-12-29 22:13:22 -0500

    Rearranges import statements so Unit may import UnitLevel

--- SpiffyRPG/SpiffyWorld/__init__.py
+++ SpiffyRPG/SpiffyWorld/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .unit_level import UnitLevel
 from .battle import Battle
 from .battle import InvalidCombatantException
 from .battle import CombatStatusException
 from .battlemaster import Battlemaster
 from .unit import Unit
 from .dungeon import Dungeon
-from .unit_level import UnitLevel
 from .effect import Effect
 from .item import Item
 from .unit_builder import UnitBuilder
